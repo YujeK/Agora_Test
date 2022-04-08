7ed9bcee3583efd6110fc1f4bbcf7ca41969eff1
YujeK <asamir-k@student.42.fr>
2022-04-08 12:19:26 +0200

    Adds CharacterController , First Person Camera Controller

---
+++ Agora_UnityProject/Assets/Resources/Script/CharacterMovement.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Transform _groundCheck;
+
+    private CharacterController _controller;
+
+    private bool _isGrounded;
+    // Update is called once per frame
+
+    [Header("Character Movement Settings")]
+    public float _speed = 5f;
+    public float _gravity = -10f;
+    public float _groundDistance = 0.4f;
+    public LayerMask _groundMask;
+    private Vector3 _velocity;
+
+    [Header("Jump")]
+    [SerializeField] private KeyCode _jumpKey;
+    public float _jumpHeight;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        GroundCheck();
+        SetMovement();
+        JumpHandler();
+        ApplyGravity();
+    }
+
+    private void JumpHandler()
+    {
+        if (Input.GetKeyDown(_jumpKey) && _isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+    }
+
+    private void GroundCheck()
+    {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        _velocity.y += _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void SetMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 direction = transform.right * x + transform.forward * z;
+        _controller.Move(direction * _speed * Time.deltaTime);
+    }
+}
---
+++ Agora_UnityProject/Assets/Resources/Script/MouseLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private Transform _characterModel;
+
+    [Header(("Mouse Settings"))]
+    [SerializeField] private float _mouseSensitivity = 100f;
+    [SerializeField] private bool _lockCursor;
+
+    private float _xRotation = 0f;
+
+    private void Start()
+    {
+        SetCursor();
+    }
+
+    // Set the Mouse Cursor State
+    private void SetCursor()
+    {
+        if (_lockCursor)
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Rotate the Camera and the character based on Mouse Inputs
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90, 90f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        _characterModel.Rotate(Vector3.up * mouseX);
+    }
+}
